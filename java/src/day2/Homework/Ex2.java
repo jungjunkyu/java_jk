8e46ef504c1669a5351b8562d3aba5825246c5bd
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-05-31 15:17:34 +0900

    과제2

---
+++ java/src/day2/Homework/Ex2.java
@@ -0,0 +1,20 @@
+package day2.Homework;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		/**
+		 * 두 정수를 입력받아 두 정수의 살술 연산 결과를 출력하는 코드를 작성하세요. 단, 2번째 숫자는 0이 아님
+		예:
+		두 정수를 입력하셍
+		1 2[엔터]
+		1 + 2 = 3
+		1 - 2 = -1
+		1 * 2 = 2
+		1 / 2 = 0.5
+		1 % 2 = 1
+		 */
+
+	}
+
+}
