6b65a8e051d5d78824767b0c0daa6b06128ffb06
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-05-31 12:40:20 +0900

    문자열 선한예제

---
+++ java/src/day2/variable/stringEx.java
@@ -0,0 +1,19 @@
+package day2.variable;
+
+public class stringEx {
+
+	public static void main(String[] args) {
+		
+		String str1 = null;
+		String str2 = "";
+		String str3 = "A";
+		String str4 = "안녕하세요";
+		
+		System.out.println(str1);//null이라는 문자열이 아님
+		System.out.println(str2);
+		System.out.println(str3);
+		System.out.println(str4);
+
+	}
+
+}
