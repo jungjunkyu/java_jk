18b17fff3251fb44294fac26019b123fe1a6a652
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-05-31 12:40:57 +0900

    리터럴예제

---
+++ java/src/day2/variable/constantEx.java
@@ -0,0 +1,22 @@
+package day2.variable;
+
+public class constantEx {
+
+	public static void main(String[] args) {
+		final int MAX_AGE;
+		//MAX_AGE : 상수, 100;리터럴
+		MAX_AGE = 100;
+		//MAX_AGE = 101; 에러발생, 1번 초기화 후 수정하려고 했기 때문
+		
+		System.out.println(MAX_AGE);
+		
+		//지역변수는 자동으로 초기화가 안됨
+		int totalCount;
+		//System.out.println(totalCount);//totalCount 변수가 초기화 되어있지 않기 때문에 사용불가-에러발생
+
+		int totlaCount = 100;
+		
+		
+	}
+
+}
