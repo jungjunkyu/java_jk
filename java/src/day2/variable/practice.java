00289b440457a2f92f9444a0a94ab36f1e4fda68
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-05-31 12:41:29 +0900

    변수 연습문제

---
+++ java/src/day2/variable/practice.java
@@ -0,0 +1,18 @@
+package day2.variable;
+
+public class practice {
+
+	public static void main(String[] args) {
+		//1. 아래 코드에서 잘못 된 부분을 찾아 수정
+		//char ch1 = "A";
+		//문자는 큰따옴표가 아닌 작은따옴표로
+		char ch1 = 'A';
+		System.out.println(ch1);
+		
+		//2. 국어 영어 수학 성적을 입력하여 구하려고한다 이때 필요한 변수들을 선언하시오.
+		int kor, eng, math; //정수를 나타나는 인트
+		double avg;
+		
+		}
+
+}
