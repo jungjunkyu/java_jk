567ff3a6963530711bad2d711d36702c67b237de
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-06-01 14:56:49 +0900

    자바 이프문 예제

---
+++ java/src/day3/ifEx/IfEx1.java
@@ -0,0 +1,22 @@
+package day3.ifEx;
+
+public class IfEx1 {
+
+	public static void main(String[] args) {
+		//정수 num가 0이면 0입니다라고 출력하는 코드
+		//정수 num가 0이다
+		//정수 num가 0과 같다
+		int num = 10;
+		if(num == 0) {
+			System.out.println("0입니다 ");
+			//0입니다라고 출력;
+		}
+		//정수 num가 0이 아니면 num is not 0라고 출력하는 코드
+		//num가 0이 아니다
+		//num가 0과 같지 않다
+		if(num != 0) {
+		System.out.println(num + " is not 0");
+		}
+		
+	}
+}
