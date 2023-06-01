c93bbe89c396739cf0309092338764ba99596e65
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-06-01 10:08:05 +0900

    Update Ex2.java

--- java/src/day2/Homework/Ex2.java
+++ java/src/day2/Homework/Ex2.java
@@ -44,8 +44,7 @@ public class Ex2 {
 		System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
 		System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
 		System.out.println(num1 + " % " + num2 + " = " + (num1 % num2));
-		
-		
+				
 		
 		sc.close();
 		
