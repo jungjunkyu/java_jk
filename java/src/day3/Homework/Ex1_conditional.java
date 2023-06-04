0089ded581a1d8bfbdfd91b533d163f699514bac
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-06-05 08:49:17 +0900

    3일차 과제

---
+++ java/src/day3/Homework/Ex1_conditional.java
@@ -0,0 +1,18 @@
+package day3.Homework;
+
+public class Ex1_conditional {
+
+	public static void main(String[] args) {
+		//성별(M:남성, W:여성)을 입력받아 여성인지 확인하는 코드를 작성.
+		//조건연산자 사용
+		/*in put gender :
+		 *M
+		 *Are you a women? false
+		 *in put gender : 
+		 *W
+		 *Are you a women? true
+		*/
+		
+	}
+
+}
---
+++ java/src/day3/Homework/Ex2_if.java
@@ -0,0 +1,10 @@
+package day3.Homework;
+
+public class Ex2_if {
+
+	public static void main(String[] args) {
+		//성별(M:남성, W:여성)을 입력받아 M이면 남성을, W이면 여성을 출력하는 코드를 작성하셍. if문 이용
+		
+	}
+
+}
