2f254280feb5f740b605982669ea802a2c5afcfc
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-06-05 15:02:18 +0900

    5일차 과제2

---
+++ java/src/day5/homework/Ex2_UpDownEx.java
@@ -0,0 +1,21 @@
+package day5.homework;
+
+public class Ex2_UpDownEx {
+
+	public static void main(String[] args) {
+		//1~100사이의 랜덤한 정수를 생성하고, 이 정수를 맞추는 게임을 구현하시오.
+		/*랜덤한 수 : 45
+		 * input : 50
+		 * Down!
+		 * 
+		 * input : 40
+		 * Up!
+		 * 
+		 * input : 45
+		 * Good!
+		 */
+		
+
+	}
+
+}
