11aca8f238ba376bdc63b7ee8267c5bc75dd3c65
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-06-05 15:00:25 +0900

    와일문 카운트넘버 숫자출력예제

---
+++ java/src/day5/whileEx/countNumberEx.java
@@ -0,0 +1,18 @@
+package day5.whileEx;
+
+public class countNumberEx {
+
+	public static void main(String[] args) {
+		/*1부터 10까지 출력하는 코드를 작성하세요.
+		 * 반복횟수 : i는 1부터 10까지 1씩 증가
+		 * 규칙성 : 1를 출력
+		 * 
+		 */
+		int i = 1;
+		while( i <= 10) {
+			System.out.println(i);
+			++i;
+		}
+	}
+
+}
