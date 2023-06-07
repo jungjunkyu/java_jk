2626c215ea5f5538614e45acf1dd5b9d120f4ab0
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-06-07 14:49:09 +0900

    6일차 과제

---
+++ java/src/day6/Homework/DisjointEx.java
@@ -0,0 +1,20 @@
+package day6.Homework;
+
+public class DisjointEx {
+
+	public static void main(String[] args) {
+		//두 정수를 입력받아 두 정수가 서로소 관계인지 판별하느 코드를 작성하세요.
+		//서로소 : 최대공약수가 1인 두 수의 관계
+		/* input 2 number : 3 4 
+		 * disjoint!(서로소)
+		 * 
+		 * input 2 number : 8 4
+		 * not disjoint!(서로소 아님)
+		 * 
+		 */
+
+		
+		
+	}
+
+}
