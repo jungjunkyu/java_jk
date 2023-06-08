ed82240ebf88192c1bbc87ec4149dd33ecfa76b7
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-06-08 11:57:22 +0900

    컨티뉴 짝수 예제

---
+++ java/src/day7/continu/evennumberEx.java
@@ -0,0 +1,32 @@
+package day7.continu;
+
+public class evennumberEx {
+
+	public static void main(String[] args) {
+		//continue 문을 이용한 10 이하의 짝수 출력 예제
+		
+		
+		int i;
+		for(i=1;i<=10;i++) {
+			//i가 홀수이면 출력하는것을 건너뜀
+			if(i%2 !=0) {
+				continue;
+				
+			}
+			System.out.println(i);
+		}
+		
+		i=0;
+		while(++i<=10) {
+			if(i%2 !=0) {
+				
+				continue;
+			}
+			System.out.println(i);
+			
+		}
+		
+		
+	}
+
+}
