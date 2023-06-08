aef3f3ca1b46851a64f882330f6c83fed7b12990
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-06-08 11:58:21 +0900

    출력 방법예제

---
+++ java/src/day7/practice/printEx.java
@@ -0,0 +1,22 @@
+package day7.practice;
+
+public class printEx {
+
+	public static void main(String[] args) {
+		//println() : 원하는 값을 출력한 후 엔터까지 입력
+		System.out.println("abc");
+		System.out.println("a");
+		System.out.println("a12");
+		
+		
+		//print() : 원하는 값을 출력. 
+		System.out.print("abc");
+		System.out.print("a");
+		System.out.print("a12");
+		
+		//printf() : 원하는 값을 출력, 서식문자를 이용
+		System.out.printf("string : %");
+
+	}
+
+}
