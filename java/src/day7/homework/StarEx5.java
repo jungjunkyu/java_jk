a3789bfd949e9460cc145f4dc90f87324edce361
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-06-08 14:43:03 +0900

    7일차 과제

---
+++ java/src/day7/homework/StarEx5.java
@@ -0,0 +1,18 @@
+package day7.homework;
+
+public class StarEx5 {
+
+	public static void main(String[] args) {
+		/*
+		 *     *    
+		 *    ***
+		 *   *****
+		 *  *******
+		 *   *****
+		 *    ***
+		 *     *
+		 */
+
+	}
+
+}
