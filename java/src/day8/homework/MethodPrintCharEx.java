6a59d32f61a22031a426daca0684aa90b274b05d
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-06-09 15:10:25 +0900

    Update MethodPrintCharEx.java

--- java/src/day8/homework/MethodPrintCharEx.java
+++ java/src/day8/homework/MethodPrintCharEx.java
@@ -5,8 +5,9 @@ public class MethodPrintCharEx {
 	public static void main(String[] args) {
 		//다음과 같이 출력하기 위한 코드를 작성하세요.
 		//메서드 이용
-		//****
-		//^^^^
+		//*****
+		//***
+		//^^^^^^
 		//????
 	}
 
