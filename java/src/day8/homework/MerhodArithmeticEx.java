34e0fab1bde513dc2686738f94aa539dbfc462b0
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-06-09 14:40:45 +0900

    8일차 과제

---
+++ java/src/day8/homework/MerhodArithmeticEx.java
@@ -0,0 +1,10 @@
+package day8.homework;
+
+public class MerhodArithmeticEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ java/src/day8/homework/MerhodMonthEx.java
@@ -0,0 +1,17 @@
+package day8.homework;
+
+public class MerhodMonthEx {
+
+	public static void main(String[] args) {
+		//월을 입력받아 월에 맞는 영어를  출력하세요.
+		//메서드 이용
+		/*
+		 * 예 :
+		 * input month :
+		 * 3
+		 * March
+		 */
+
+	}
+
+}
