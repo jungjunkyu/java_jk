abb0df15f6686ca2aad71b058b83f033c558ce3f
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-06-09 09:28:29 +0900

    Update StarEx5.java

--- java/src/day7/homework/StarEx5.java
+++ java/src/day7/homework/StarEx5.java
@@ -4,15 +4,29 @@ public class StarEx5 {
 
 	public static void main(String[] args) {
 		/*
-		 *     *    
-		 *    ***
-		 *   *****
-		 *  *******
-		 *   *****
-		 *    ***
-		 *     *
+		 *     *    	i=1 공=3 *=1
+		 *    ***		i=2 공=2 *=3
+		 *   *****		i=3 공=1 *=5		
+		 *  *******		i=4 공=0 *=7
+		 *   *****		i=5 공=2 *=5
+		 *    ***		i=6 공=3 *=3
+		 *     *		i=7 공=3 *=1
 		 */
+		
+		int num =7;
+		for(int i=1;i<=num;i++) {
+			int half=(num +1)/2;
+			//4줄 => (num+1)/2
+				
+			
 
+			//엔터
+			System.out.println();
+		
+
+		
+		}
+		
 	}
 
 }
