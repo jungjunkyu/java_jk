1e37b5aa0a794b4f5e01006ecd3a50c5db6031d4
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-06-09 15:19:37 +0900

    Update MethodScoreEx.java

--- java/src/day8/homework/MethodScoreEx.java
+++ java/src/day8/homework/MethodScoreEx.java
@@ -1,11 +1,28 @@
 package day8.homework;
 
+import java.util.Scanner;
+
 public class MethodScoreEx {
 
 	public static void main(String[] args) {
 		//성적을 입력받아 학점을 출력하는 코드를 작성하세요.
 		//메서드 이용
 
+		int num;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("input Score : ");
+		num = sc.nextInt();
+		
+		//90이상 100점 : A
+		//80이상 90미만 : B
+		//70이상 80미만 : C
+		//60이상 70미만 : D
+		//나머지 : fail
+		
+		sc.close();
 	}
 
+	public static void score(int num) {
+		if()
+	}
 }
