aadcf09229abd3c6397590633a52983376f4c4ce
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-06-09 14:29:14 +0900

    Update MethodEvenNumberEx.java

--- java/src/day8/practice/MethodEvenNumberEx.java
+++ java/src/day8/practice/MethodEvenNumberEx.java
@@ -5,78 +5,84 @@ import java.util.Scanner;
 public class MethodEvenNumberEx {
 
 	public static void main(String[] args) {
-		/*정수 num를 입력받아 짝수인지 아닌지 판별하는 코드를 작성하세요.
-		 *단, 메서드를 이용할 것
-		 */
-		
+		//홀짝 판별 예제. 메서드 이용
 		int num1;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("input number : ");
 		num1 = sc.nextInt();
 		
-		public static void mod(int num1) {
-			if(num1%2==0);{
-				System.out.println("짝수");
-							
-			}else {
-		    	System.out.println("홀수");
-		    }
-			printEvenNumber(num1);
-			if(mod(num1,2)==0) {
-				System.out.println("even number");
-			}else {
-				System.out.println("odd number");
-			}
-			
-		/**num가 주어지면 num가 짝수인지 홀수 인지 출력하는 메서드
-		 * @param num : 판별할 정수
-		 * @return 없음void
-		 * 메서드 명 : printEvenNumber
-		 */
-			
-		public static void printEvenNumber(int num) {
-			if(num%2==0);{
-				System.out.println("even number");
-							
-			}else {
-		    	System.out.println("odd number");
-		    	
-		    if(isEvenNumber(num1)) {
-		    	System.out.println("even number");
-				
-			}else {
-		    	System.out.println("odd number");
-			}
-		    	
+		if(num1 % 2 == 0) {
+			System.out.println("Even number.");
+		}else {
+			System.out.println("Odd number.");
 		}
-		/**
-		 * num1과 num2가 주어지면 num1을 num2로 나누었을 때 나머지를 
-		 * 알려주는 메서드
-		 * 매개변수 : 두 정수 =>int num1, int num2
-		 * 리턴타입 : 나누었을 때 나머지 => 정수
-		 * 메서드명 : mod
-		 */
+		
+		printEvenNumber(num1);
+		
+		if(mod(num1, 2) == 0) {
+			System.out.println("Even number.");
+		}else {
+			System.out.println("Odd number.");
+		}
+		
+		if(isEvenNumber(num1)) {
+			System.out.println("Even number.");
+		}else {
+			System.out.println("Odd number.");
+		}
+		
+		if(isMultiple(num1, 2)) {
+			System.out.println("Even number.");
+		}else {
+			System.out.println("Odd number.");
+		}
+		
+		sc.close();
 	}
-		public static int mod(int num1, int num2) {
-			return num1 % num2;
+	/**num가 주어지면 num가 짝수인지 홀수인지 출력하는 메서드
+	 * @param num : 판별할 정수 
+	 * @return 없음 => void
+	 * 메서드명 : printEvenNumber 
+	 */
+	public static void printEvenNumber(int num) {
+		if(num % 2 == 0) {
+			System.out.println("Even number.");
+		}else {
+			System.out.println("Odd number.");
 		}
-		/**
-		 * num가 주어지면 num가 짝수인지 홀수인지 알려주는 메서드
-		 * 매개변수 : 판별할 정수 => int num
-		 * 리턴타입 : 짝수이지(참) 홀수인지(거짓)=>boolean
-		 * 매서드명 : isEvenNumber
-		 */
-		public static boolean isEvenNumber(int num) {
-			return num%2==0;
-			
-		}
-		/**num1과 num2가 주어지면 num1이 num2의 배수인지 아닌지 알려주는
-		 * 메서드
-		 * 매개변수 : 두정수 int num1, int num2
-		 * 리턴타입 : 배수인지(참) 아닌지(거짓) => boolean
-		 * 메서드명 : isMultiple
-		 */
-		public static boolean isMultiple(int num1, int num2) {
+	}
+	/**num1과 num2가 주어지면 num1을 num2로 나누었을 때 나머지를
+	 * 알려주는 메서드
+	 * 매개변수 : 두 정수 => int num1, int num2
+	 * 리턴타입 : 나누었을 때 나머지 => 정수 => int
+	 * 메서드명 : mod
+	 * 
+	 */
+	public static int mod(int num1, int num2) {
+		return num1 % num2;
+	}
+	/**num가 주어지면 num가 짝수인지 홀수인지 알려주는 메서드
+	 * 매개변수 : 판별할 정수 => int num
+	 * 리턴타입 : 짝수인지(참) 홀수인지(거짓) => boolean
+	 * 메서드명 : isEvenNumber
+	 */
+	public static boolean isEvenNumber(int num) {
+		return num % 2 == 0;
+	}
+	/**num1과 num2가 주어지면 num1이 num2의 배수인지 아닌지 알려주는 
+	 * 메서드
+	 * 매개변수 : 두 정수 => int num1, int num2
+	 * 리턴타입 : 배수인지(참) 아닌지(거짓) => boolean
+	 * 메서드명 : isMultiple
+	 */
+	public static boolean isMultiple(int num1, int num2) {
+		return num1 % num2 == 0;
+	}
 }
+
+
+
+
+
 
 
