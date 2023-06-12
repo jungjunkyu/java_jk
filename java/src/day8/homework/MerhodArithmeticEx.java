c0d5da119ca144007cfe2d0a61cfd463b587b123
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-06-12 12:43:50 +0900

    메서드 정수 연산자 출력 예제

--- java/src/day8/homework/MerhodArithmeticEx.java
+++ java/src/day8/homework/MerhodArithmeticEx.java
@@ -1,5 +1,7 @@
 package day8.homework;
 
+import java.util.Scanner;
+
 public class MerhodArithmeticEx {
 
 	public static void main(String[] args) {
@@ -8,7 +10,80 @@ public class MerhodArithmeticEx {
 		//연산자를 잘못 입력한 경우는 없다고 가정.
 		//0으로 나누는 일은 없다고 가정.
 		
-
+		
+		int num1, num2;
+		char operator;
+		double res;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("input(ex : 1 + 2) : ");
+		num1=sc.nextInt();
+		operator = sc.next().charAt(0);
+		num2=sc.nextInt();
+		
+		res=arithmetic(num1,operator,num2);
+		System.out.println(num1+" "+operator+" "+num2+" "+res);
+		
+		res=arithmetic2(num1,operator,num2);
+		System.out.println(num1+" "+operator+" "+num2+" "+res);
+		
+		
+		
 	}
 
+	/** 두 정수와 산술 연산자가 주어지면 산술 연산겨로가를 알려주는 메서드
+	 * 매개변수 : 두 정수와 산술연산자 => int num1, char operator, int num2
+	 * 리턴타입 : 산술연산 결과 => 실수 => double
+	 * 메서드명 : arithmetic 
+	 * 
+	 */
+	public static double arithmetic(int num1, char operator, int num2) {
+		double res = 0.0;
+		switch(operator){
+		case '+':
+			res = num1+num2;
+			break;
+		case '-':
+			res = num1-num2;
+			break;
+		case '*':
+			res = num1*num2;
+			break;
+		case '/':
+			res = (double)num1/num2;
+			break;
+		case '%':
+			res = num1%num2;
+			break;
+		default:
+			//잘못된 연산자인 경우 예외처리를 해야하는데 아직 안배워서 주석처리
+			//throw new RuntimeException("Wrong operator!")
+				
+			
+		}
+		return res;
+	}
+	public static double arithmetic2(int num1, char operator, int num2) {
+		double res = 0.0;
+		switch(operator){
+		case '+':
+			return num1+num2;
+			
+		case '-':
+			return num1-num2;
+		
+		case '*':
+			return num1*num2;
+		
+		case '/':
+			return (double)num1/num2;
+		
+		case '%':
+			return num1%num2;
+		
+		default:
+			//잘못된 연산자인 경우 예외처리를 해야하는데 아직 안배워서 주석처리
+			//throw new RuntimeException("Wrong operator!")
+			return 0.0;	
+		}
+	}
 }
