9c1fe79b9ee6c1a49aa7c261f5b29b82537f3d2f
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-06-12 12:45:00 +0900

    메서드 성적입력 학점출력

--- java/src/day8/homework/MethodScoreEx.java
+++ java/src/day8/homework/MethodScoreEx.java
@@ -8,21 +8,50 @@ public class MethodScoreEx {
 		//성적을 입력받아 학점을 출력하는 코드를 작성하세요.
 		//메서드 이용
 
-		int num;
-		Scanner sc=new Scanner(System.in);
-		System.out.println("input Score : ");
-		num = sc.nextInt();
+		double score = 100;
+		System.out.println(getGrade(score));
 		
-		//90이상 100점 : A
-		//80이상 90미만 : B
-		//70이상 80미만 : C
-		//60이상 70미만 : D
-		//나머지 : fail
 		
-		sc.close();
+		
+		
+	}	
+	/**성적이 주어지면 학점을 알려주는 메서드
+	 * 매개변수 : 성적 => 실수 => double
+	 * 리턴타입 : 학점 => 문자열 => String
+	 * 메서드명 : getGrade
+	 */
+	
+	public static String getGrade(double score) {
+		if(score > 100 || score <0) {
+			return "Wrong score!";
+		}
+		/* 100점 => A
+		 * 90점대 => A
+		 * 80점대 => B
+		 * 70점대 => C
+		 * 60점대 => D
+		 * 그 외 => F
+		 */
+		//성적이 몇점대인지 확인하기 위한 정수
+		//9가 나오면 90점대
+		int res=(int)(score/10);
+		switch(res) {
+		case 10:
+		case 9 :
+			return "A";
+		case 8 :
+			return "B";
+		case 7 :
+			return "C";
+		case 6 :
+			return "D";
+		default :
+			return "F";
+			
+			
+		}
 	}
-
-	public static void score(int num) {
-		if()
-	}
+	
+	
 }
+
