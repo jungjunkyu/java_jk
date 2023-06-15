09dbb6bbc49cd453030c62a8f6fc8f231bd370c2
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-06-15 14:58:57 +0900

    자바 로또 과제

---
+++ java/src/day12/homework/LottoEx.java
@@ -0,0 +1,13 @@
+package day12.homework;
+
+public class LottoEx {
+
+	public static void main(String[] args) {
+		//로또 번호를 생성하고, 사용자가 번호를 입력하면 몇등인지 출력하는 코드를 작성하세요.
+		/*
+		 * 1등 : 번호 6개
+		 */
+
+	}
+
+}
