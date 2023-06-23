8c115e8c9d58d5384ba5f4a0695e605abd0df72f
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-06-23 14:53:03 +0900

    18일 과제

---
+++ java/src/day18/homework/ImgEx.java
@@ -0,0 +1,23 @@
+package day18.homework;
+
+import java.util.Scanner;
+
+public class ImgEx {
+
+	public static void main(String[] args) {
+		//파일명을 입력받아 파일이 이미지인지 확인하는 코드를 작성하세요.
+		/* 이미지 확장자 : jpg, bmp, png(더 있지만 간략하게 3개만 체크)
+		 * 
+		 * input : dog.jpg
+		 * dog.jpg  : Image
+		 * 
+		 * input : dog.exe
+		 * dog.exe : Not image
+		 * 
+		 */
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
+
+}
