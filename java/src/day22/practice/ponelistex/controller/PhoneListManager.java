e8e68cf704ef10642526c5331ab3a48d8967c93d
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-06-29 15:08:13 +0900

    22일과제

---
+++ java/src/day22/practice/ponelistex/controller/PhoneListManager.java
@@ -0,0 +1,22 @@
+package day22.practice.ponelistex.controller;
+
+import java.util.Scanner;
+
+public class PhoneListManager {
+
+	public static void main(String[] args) {
+		
+		
+		}
+	public void printMenu() {
+		System.out.println("1.전화번호 등록");
+		System.out.println("2.전화번호 수정");
+		System.out.println("3.전화번호 검색");
+		System.out.println("4.전화번호 종료");
+	}
+	public void run() {
+		System.out.println();
+		
+	}
+
+}
---
+++ java/src/day22/practice/ponelistex/controller/Program.java
@@ -0,0 +1,8 @@
+package day22.practice.ponelistex.controller;
+
+public interface Program {
+	void printMenu();
+	void run();
+	void runMenu(int menu);
+
+}
