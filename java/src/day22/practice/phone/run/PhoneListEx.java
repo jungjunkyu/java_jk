76f2d3aedf669ff0c64ac31b39a91db8e1a5e278
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-06-30 09:16:39 +0900

    수정

---
+++ java/src/day22/practice/phone/run/PhoneListEx.java
@@ -0,0 +1,10 @@
+package day22.practice.phone.run;
+
+public class PhoneListEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- java/src/day22/practice/ponelistex/controller/Program.java
+++
@@ -1,8 +0,0 @@
-package day22.practice.ponelistex.controller;
-
-public interface Program {
-	void printMenu();
-	void run();
-	void runMenu(int menu);
-
-}
