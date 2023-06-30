513d25b8b1800d74bda1b5422889310c4edaca77
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-06-30 15:14:53 +0900

    Update PhoneNumber.java

--- java/src/day22/practice/phone/vo/PhoneNumber.java
+++ java/src/day22/practice/phone/vo/PhoneNumber.java
@@ -26,7 +26,8 @@ public class PhoneNumber {
 		 } */
 	}
 	//전화번호가 010-xxxx-xxxx 패턴이 맞는지 확인해주는 메서드
-	private boolean checkNumber(String num) {
+	//외부에서도 객체생성없이 쓸 수 있게 static과 public을 붙여줌
+	public static boolean checkNumber(String num) {
 		String regex = "^010(-\\d{4}){2}$";
 		return Pattern.matches(regex, num);
 	}
