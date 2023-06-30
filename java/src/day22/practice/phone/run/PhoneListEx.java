e83ca77afd9324d798d82634ad9397bc90687e47
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-06-30 09:52:18 +0900

    22일 폰리스트 예제

--- java/src/day22/practice/phone/run/PhoneListEx.java
+++ java/src/day22/practice/phone/run/PhoneListEx.java
@@ -1,10 +1,15 @@
 package day22.practice.phone.run;
 
+import day22.practice.phone.vo.PhoneBook;
+
 public class PhoneListEx {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		PhoneBook pb = new PhoneBook();
+		System.out.println(pb.insertPhone("Hong", "010-1234-5678"));
+		System.out.println(pb.insertPhone("Bong", "010-234-5678"));
+		pb.print(p->true);
+		
 	}
 
 }
