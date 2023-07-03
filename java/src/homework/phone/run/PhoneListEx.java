696966f8ca36e4870c48113f67dcddf6539d3d09
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-07-03 15:12:12 +0900

    홈워크

---
+++ java/src/homework/phone/run/PhoneListEx.java
@@ -0,0 +1,13 @@
+package homework.phone.run;
+
+import homework.phone.controller.PhoneManager;
+
+public class PhoneListEx {
+
+	public static void main(String[] args) {
+
+		PhoneManager pm = new PhoneManager();
+		pm.run();
+	}
+
+}
