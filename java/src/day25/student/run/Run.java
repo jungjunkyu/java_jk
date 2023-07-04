d45dd342c91d576a4c17269ba6877a4e9698ac62
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-07-04 15:02:21 +0900

    대학생 정보관리 프로그램 예제

---
+++ java/src/day25/student/run/Run.java
@@ -0,0 +1,13 @@
+package day25.student.run;
+
+import day25.student.controller.StudentManager3;
+
+public class Run {
+
+	public static void main(String[] args) {
+		StudentManager3 sm = new StudentManager3();
+		sm.run();
+
+	}
+
+}
