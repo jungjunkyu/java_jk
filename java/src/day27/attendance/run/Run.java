c60638d4c3381c201826adb0f0a0a9e5960fabec
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-07-06 16:49:29 +0900

    출석부 프로그램 예제

---
+++ java/src/day27/attendance/run/Run.java
@@ -0,0 +1,13 @@
+package day27.attendance.run;
+
+import day27.attendance.controller.AttendanceController;
+
+public class Run {
+
+	public static void main(String[] args) {
+		AttendanceController ac = new AttendanceController();
+		ac.run();
+
+	}
+
+}
