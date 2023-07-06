33db9fd868912e67d644b822e3e5ececfbf37d3f
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-07-06 16:50:13 +0900

    문자단위 IOStream예제

---
+++ java/src/day27/iostream/CharIOStreamEx.java
@@ -0,0 +1,34 @@
+package day27.iostream;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CharIOStreamEx {
+
+	public static void main(String[] args) {
+		//문자단위로 읽어오는 예제
+		try(FileReader fr = new FileReader("student.txt")){
+			char buffer[] = new char[1024];
+			while(fr.read(buffer)!=-1) {
+				System.out.print(buffer);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found");
+		} catch (IOException e) {
+			System.out.println("Exception");
+		}
+		//문자단위로 쓰는 예제
+		//FileWriter생성자(파일명,이어쓰기 여부) : 이어쓰기 여부가 true이면 이어서 작성
+		//FileWriter생성자(파일명) : 이어쓰기 여부가 false로 설정
+		try(FileWriter fw = new FileWriter("file.txt", true)){
+			String str = "Hello world!\n";
+			fw.write(str);
+			String str2 = "Hi, Java!\n";
+			fw.write(str2);
+		}catch (IOException e) {
+			System.out.println("Exception");
+		}
+	}
+}
