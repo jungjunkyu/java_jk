747b128518ad7e1022799c66726692ccd015d68b
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-09-07 12:28:29 +0900

    Create MemberService.java

---
+++ spring project/study/src/main/java/kr/kh/study/service/MemberService.java
@@ -0,0 +1,12 @@
+package kr.kh.study.service;
+
+import kr.kh.study.vo.MemberVO;
+
+public interface MemberService {
+
+	boolean signup(MemberVO member);
+
+	MemberVO login(MemberVO member);
+
+	
+}
