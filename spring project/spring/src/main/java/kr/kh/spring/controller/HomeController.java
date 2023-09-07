297ceb6a236d085265b929df5ba2c1799a331827
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-09-07 12:30:07 +0900

    Update HomeController.java

--- spring project/spring/src/main/java/kr/kh/spring/controller/HomeController.java
+++ spring project/spring/src/main/java/kr/kh/spring/controller/HomeController.java
@@ -1,32 +1,38 @@
 package kr.kh.spring.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import kr.kh.spring.vo.MemberVO;
 
 @Controller
 public class HomeController {
-
-	@RequestMapping(value = "/")
+	
+	@RequestMapping(value="/")
 	public String home(Model model) {
-		//È­¸é¿¡ µ¥ÀÌÅÍ¸¦ Àü¼ÛÇÏ´Â ¹æ¹ı model¿¡ ´ã¾Æ¼­ Àü¼Û
-		//model.addAttribute("È­¸é¿¡¼­ »ç¿ëÇÒ ÀÌ¸§", Àü¼ÛÇÒ°ª);
 		model.addAttribute("name", "abc");
-		return "home";
+		return "/main/home";
 	}
 	
-	@RequestMapping(value = "/test1")
+	@RequestMapping(value="/test1")
 	public String test1(Model model, String id, Integer age) {
-		System.out.println("¾ÆÀÌµğ : " + id);
-		System.out.println("³ªÀÌ : " + age);
+		System.out.println("ì•„ì´ë”” : " +id);
+		System.out.println("ë‚˜ì´ : " + age);
 		return "test1";
+	}
+	@ResponseBody
+	@PostMapping("/ajax/test")
+	public Map<String, Object> ajaxTest(@RequestBody MemberVO member){
+		Map<String, Object> map = new HashMap<String, Object>();
+		System.out.println(member);
+		map.put("name", "í™ê¸¸ë™");
+		return map;
 	}
 }
