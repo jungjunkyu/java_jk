824a300ea529d04d691c2b8daad2a22582a16ac1
jungjunkyu <134986519+jungjunkyu@users.noreply.github.com>
2023-09-08 16:46:34 +0900

    세미프로젝트쇼핑몰

--- spring project/semi-project/src/main/java/kr/kh/semiproject/controller/MemberController.java
+++ spring project/semi-project/src/main/java/kr/kh/semiproject/controller/MemberController.java
@@ -2,6 +2,7 @@ package kr.kh.semiproject.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import kr.kh.semiproject.service.MemberService;
 
@@ -11,5 +12,9 @@ public class MemberController {
 	@Autowired
 	private MemberService memberService;
 
+	@GetMapping("/member/signup")
+	public String memberSignup() {
+		return "member/signup";
+	}
 	
 }
